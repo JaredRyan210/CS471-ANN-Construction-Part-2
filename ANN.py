65613e113ed06416795cb3d61b792a49e7abfb5c
JRyan109 <124639959+JRyan109@users.noreply.github.com>
2023-04-21 13:15:46 -0700

    Update ANN.py

--- ANN.py
+++ ANN.py
@@ -25,7 +25,8 @@ def read_csv(in_file):
   with open(in_file, 'r') as file:
     contents = csv.reader(file)
     for line in contents:
-       inputs.append([float(x) for x in line]) ###[:-1] to get 4 inputs
+      inputs.append([float(x) for x in line]) ###[:-1] to get 4 inputs
+    #print(inputs)
   return inputs
 
 def init_network(structure):
@@ -48,7 +49,7 @@ def forward_prop(inputs, network):
       for conn_index, conn in enumerate(node.connections):
         weight = random.uniform(0,1)
 
-        node.collector += conn.collector * weight
+        node.collector = conn.collector + (conn.collector * weight)
         node.collector = transfer(node.collector)
         
         #print(f"weight of: {layer_index + 1}, {node_index + 1}, {conn_index + 1}:", weight)
@@ -84,34 +85,39 @@ def backward_propagate_error(network, expected):
     for j in range(len(layer)):
       neuron = layer[j]
       neuron.delta = errors[j] * transfer_deriv(neuron.collector)
+      #print('delta',neuron.delta)
 
 
 ###########FIX AND FINISH 4/19###################
 def update_weights(l_rate):
   for i in range(1, len(network)):
     inputs = [neuron.collector for neuron in network[i - 1]]
-    for node in network[i]:
+    for neuron in network[i]:
       for j in range(len(inputs)):
-        node.connections[j].weight += l_rate * node.delta * inputs[j]
-      node.connections[-1].weight += l_rate * node.delta
+        neuron.connections[j].weight -= l_rate * neuron.delta * inputs[j]
+      neuron.connections[-1].weight -= l_rate * neuron.delta
 
 def train_network(network, train, l_rate, n_epoch, target_error):
   num_inputs = len(network[0])
   for epoch in range((n_epoch)):
-    sum_error = 0
+    sum_error = 0.0
     for row in train:
       inputs = row[:num_inputs]
       excepted = row[num_inputs:]
+      #print(excepted)
       outputs = forward_prop(inputs, network)
+      #print('Inputs:',inputs)
+      #print('Expected:', excepted)
       for i in range(len(excepted)):
+        #print(excepted[i], '-', outputs[i])
         sum_error += (excepted[i] - outputs[i]) ** 2
         #print("sum error:", sum_error)
-      if sum_error <= target_error:
+      if sum_error <= (target_error):
         print("Target error reached...error r=%f" % (sum_error))
         return
       backward_propagate_error(network, excepted)
       update_weights(l_rate)
-    print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+    print('>epoch=%d, lrate=%.2f, error=%.3f' % (epoch, l_rate, sum_error))
 
 
 
@@ -149,6 +155,6 @@ if __name__ == '__main__':
 
   network = init_network(structure)
 
-  train_network(network, inputs, l_rate = 0.5, n_epoch=10, target_error = 0.05)
+  train_network(network, inputs, l_rate = 0.05, n_epoch=20, target_error = 0.05)
 
   ###############
