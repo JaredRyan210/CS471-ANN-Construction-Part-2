6c64b54f4da32df91562c9ed3fb4cf5727649b1f
JRyan109 <124639959+JRyan109@users.noreply.github.com>
2023-04-28 16:07:03 -0700

    Update ANN.py

--- ANN.py
+++ ANN.py
@@ -10,6 +10,7 @@ class Node:
         self.weight = random.uniform(0,1)
 
 
+
 def read_network(in_file):
   with open(in_file, 'r') as file:
     contents = file.read().strip().split(',')
@@ -22,6 +23,7 @@ def read_csv(in_file):
     contents = csv.reader(file)
     for line in contents:
       inputs.append([float(x) for x in line]) ###[:-1] to get 4 inputs
+      expected.append([float(x) for x in line[-1]])
     #print(inputs)
   return inputs
 
@@ -40,17 +42,17 @@ def forward_prop(inputs, network):
     network[0][i].collector = inputs[i]
   #####RUN_INPUT######
   for layer in (range(1,len(network))):
+    #print(f"layer inputs {inputs}")
     for node in (network[layer]):
-      #node.collector = 0.0
+      node.collector = 0.0
       for conn in (node.connections):
         #weight = random.uniform(0,1)
-
+        #print("Input:",inputs[i + 1])        
+        #print("Initial weight:", conn.weight)
         node.collector = conn.collector + (conn.collector * conn.weight)
+
+        #print("collector:",node.collector)
       node.collector = transfer(node.collector)
-        
-        #print(f"weight of: {layer_index + 1}, {node_index + 1}, {conn_index + 1}:", weight)
-        #print(f"value at collector:{layer_index + 1}, {node_index + 1}, {conn_index + 1}:", node.collector)
-      #print("\n")
       
   output = []
   for node in network[-1]:
@@ -59,7 +61,7 @@ def forward_prop(inputs, network):
   return output
 
 def transfer(activation):
-  return 1.0 / (1.0 + math.exp(-activation))
+  return 1.0 / (1.0 + math.exp(-(activation)))
 
 def transfer_deriv(output):
   return output * (1.0 - output)
@@ -85,34 +87,48 @@ def backward_propagate_error(network, expected):
 
 
 
-def update_weights(l_rate):
+def update_weights(network, l_rate):
   for i in range(1, len(network)):
     inputs = [neuron.collector for neuron in network[i - 1]]
     for neuron in network[i]:
       for j in range(len(inputs)):
         neuron.connections[j].weight -= l_rate * neuron.delta * inputs[j]
-      neuron.connections[-1].weight -= l_rate * neuron.delta
-        
+        #print("updated weight:",neuron.connections[j].weight)
+
+    neuron.connections[-1].weight -= l_rate * neuron.delta
 
 
 
 def train_network(network, train, l_rate, n_epoch, target_error):
   num_inputs = len(network[0])
   for epoch in range((n_epoch)):
-    sum_error = 0.0
+    sum_error = 0
     for row in train:
+      #print(row)
       forward_prop(row, network)
       expected = []
       for i in range(len(network[-1])):
         expected.append(row[num_inputs + i])
+        #print(expected[i], "-", network[-1][i].collector, "**2", sum_error)
+        error = (expected[i] - network[-1][i].collector)
+        sum_error += error ** 2
+        #sum_error += (expected[i] - network[-1][i].collector) ** 2
         #print('expected:', expected)
-        sum_error += (row[num_inputs + i] - network[-1][i].collector) ** 2
-        if sum_error <= (target_error):
-          print("Target error reached...error r=%f" % (sum_error))
-          return
+        #print("sum_error", sum_error)
+        #print("row[num_inputs + i]:", row[num_inputs + i])
+        #print("network[-1][i].collector:",network[-1][i].collector)
+        
+        #print("sum error total: ",sum_error)
+      if sum_error <= (target_error):
+        print("Target error reached...error r=%f" % (sum_error))
+        return     
       backward_propagate_error(network, expected)
-      update_weights(l_rate)
+      update_weights(network,l_rate)          
     print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
+
+           
+
+    
 
 if __name__ == '__main__':
 
@@ -125,6 +141,6 @@ if __name__ == '__main__':
 
   network = init_network(structure)
 
-  train_network(network, inputs, l_rate = 0.05, n_epoch=100, target_error = 0.05)
+  train_network(network, inputs, l_rate= 0.05, n_epoch=100, target_error= 0.05)
 
   ###############
